1b605aa1bfb8e2c8ba32c15e8adaa82dd4210b76
GlennOngJunJie <glennongjj@gmail.com>
2024-06-03 17:51:44 +0800

    Added launching game feature. TBD: Select TFT, Start TFT

--- Autoclicker.py
+++ Autoclicker.py
@@ -1,11 +1,69 @@
 import time
 import pyautogui as pgi
+import cv2
 
-while True:
+pixelRatio = pgi.screenshot().size[0]/pgi.size().width
+
+while True: # launch game
+    try:
+        movement = pgi.position()
+        pgi.moveTo(movement[0], 1800)
+        point = pgi.locateCenterOnScreen('/Users/glenn/AutoClicker/Image/launch.png', confidence=0.6)
+        if point is not None:
+            pgi.moveTo(point[0]/pixelRatio,point[1]/pixelRatio)
+            pgi.click()
+            time.sleep(5)
+            break
+        else:
+            time.sleep(1)  # Adding a small delay to prevent the loop from running too quickly
+    except pgi.ImageNotFoundException:
+        print("League has not been launched...")
+        time.sleep(1) 
+
+while True: # select TFT not done
+    try:
+        point = pgi.locateCenterOnScreen('/Users/glenn/AutoClicker/Image/launch.png', confidence=0.6)
+        if point is not None:
+            pgi.moveTo(point[0]/pixelRatio,point[1]/pixelRatio)
+            pgi.click()
+            break
+        else:
+            time.sleep(1)  # Adding a small delay to prevent the loop from running too quickly
+    except pgi.ImageNotFoundException:
+        print("League has not been switched to tft...")
+        time.sleep(1) 
+
+while True: # select ranked
+    try:
+        point = pgi.locateCenterOnScreen('/Users/glenn/AutoClicker/Image/launch.png', confidence=0.6)
+        if point is not None:
+            pgi.moveTo(point[0]/pixelRatio,point[1]/pixelRatio)
+            pgi.click()
+            break
+        else:
+            time.sleep(1)  # Adding a small delay to prevent the loop from running too quickly
+    except pgi.ImageNotFoundException:
+        print("League has not been launched...")
+        time.sleep(1) 
+
+while True: # start ranked
+    try:
+        movement = pgi.position()
+        pgi.moveTo(movement[0], 1800)
+        point = pgi.locateCenterOnScreen('/Users/glenn/AutoClicker/Image/launch.png', confidence=0.6)
+        if point is not None:
+            pgi.moveTo(point[0]/pixelRatio,point[1]/pixelRatio)
+            pgi.click()
+            break
+        else:
+            time.sleep(1)  # Adding a small delay to prevent the loop from running too quickly
+    except pgi.ImageNotFoundException:
+        print("League has not been launched...")
+        time.sleep(1) 
+
+while True: # queue acceptance
     try:
         point = pgi.locateCenterOnScreen('/Users/glenn/AutoClicker/Image/accept.png', confidence=0.7)
-        print(point)
-        pixelRatio = pgi.screenshot().size[0]/pgi.size().width
         if point is not None:
             pgi.moveTo(point[0]/pixelRatio,point[1]/pixelRatio)
             pgi.click()
@@ -13,7 +71,7 @@ while True:
         else:
             time.sleep(3)  # Adding a small delay to prevent the loop from running too quickly
     except pgi.ImageNotFoundException:
-        print("Image not found on screen. Retrying...")
+        print("Queue not found yet...")
         time.sleep(1) 
 
 print("end")
